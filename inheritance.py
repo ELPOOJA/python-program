09b003ac4c8e1295f90d568ef9fe9b740f246dba
ELPOOJA <el_pooja@yahoo.com>
2014-01-27 19:58:57 +0530

    Create inheritance.py

---
+++ inheritance.py
@@ -0,0 +1,17 @@
+class student:
+name=' '
+Mark=0
+def display(self)
+print("student name is{0}\n student mark is {1}." form at("self.name,self.mark")
+obj=student( )
+obj.name="xxx"
+obj.mark="80"
+obj.display()
+ class sports(student):
+  game=''
+  rank=0
+ def sleep rank(self)
+  print("the game is {0}\n rank is{1}".format("self.name,self.rank")
+obj=sports()
+obj.game="athlet"
+obj.rank="20"
